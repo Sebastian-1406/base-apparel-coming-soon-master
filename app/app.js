26dc5f7e8eb000fd9d16214a2fd27b23bae4ada3
Sebastian <SCsebastian1406@gmail.com>
2021-10-16 22:57:20 -0500

    agregado normalize, diseño desktop, corregido problemas de accesibilidad

--- app/app.js
+++ app/app.js
@@ -1,31 +1,18 @@
-let botonEnviar = document.getElementById("boton");
-let email = document.getElementById("email");
-
+let formulario = document.getElementById("formulario");
+let botonEnviar = document.getElementById("btn_enviar");
+let email = document.getElementById("email")
 const expresiones = {correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,}
 
-const validarCampo = () => {
-    let formulario = document.getElementById("formulario");
-    let expresionEmail = expresiones.correo.test(email.value)
+const validarCampo = (e) => {
+    e.preventDefault()
 
-    if(expresionEmail){
-        formulario.classList.remove("mensaje-incorrecto");
-        expresionEmail = true;
-    }else{
-        formulario.classList.add("mensaje-incorrecto");
-        expresionEmail = false;
+    if(expresiones.correo.test(email.value)){
+        formulario.classList.remove("campo__incorrecto")
+        formulario.reset()
+    }else {
+        formulario.classList.add("campo__incorrecto")
     }
-
-    formulario.addEventListener("submit", (e) => {
-        e.preventDefault();
-        if(expresionEmail){
-            formulario.reset()
-        }
-
-    })
 
 }
 
-
 botonEnviar.addEventListener("click", validarCampo)
-
-
